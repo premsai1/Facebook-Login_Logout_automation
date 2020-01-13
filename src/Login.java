a591c4f852c91b16af421f5c62513b0f61aa99a9
Prem <prem@Prems-MacBook-Air.local>
2020-01-13 12:37:14 +0530

    updated :Naming Convention

---
+++ src/Login.java
@@ -0,0 +1,34 @@
+import java.awt.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Login {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"/home/user/Desktop/selenium/drivers/chromedriver_linux64/chromedriver");
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--disable-notifications");
+
+		WebDriver driver = new ChromeDriver(option);
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("mpremsai");
+		Thread.sleep(3000);
+		driver.findElement(By.id("pass")).sendKeys("*******");
+		Thread.sleep(3000);
+		driver.findElement(By.id("loginbutton")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement accountSettings = wait
+				.until(ExpectedConditions.elementToBeClickable(By.linkText("Account Settings")));
+		accountSettings.click();
+		WebElement logOut = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log Out")));
+		logOut.click();
+
+	}
+}
---
+++ src/Registeration.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Registeration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"/home/user/Desktop/selenium/drivers/chromedriver_linux64/chromedriver");
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(option);
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.className("firstname")).sendKeys("premsai");
+		driver.findElement(By.xpath("//input[@id='u_0_s']")).sendKeys("manthani");
+		driver.findElement(By.xpath("//input[@id='u_0_v']")).sendKeys("manthanipremsai@gmail.com");
+		driver.findElement(By.xpath("//input[@id='u_0_10']")).sendKeys("*******");
+		
+	
+		
+	}
+
+}
